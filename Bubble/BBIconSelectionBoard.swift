644b8ac3b43cb163daddbee77edfe6612d122afd
AmyLiu11 <liuxiaofen11@gmail.com>
2014-09-28 18:57:29 -0400

    editor

--- Bubble/BBIconSelectionBoard.swift
+++ Bubble/BBIconSelectionBoard.swift
@@ -11,7 +11,10 @@ import UIKit
 
 class BBIconSelectionBoard: UIView{
     
-    
+    var originX:CGFloat = 0
+    var originY:CGFloat = 0
+    var originGapx:CGFloat = 20
+    var originGapy:CGFloat = 20
     func generateButton(type:BBBoardType){
         
     }
