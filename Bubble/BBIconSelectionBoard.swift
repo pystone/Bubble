d86cc21501892544406ae2143f33ce7cffdca01f
AmyLiu11 <liuxiaofen11@gmail.com>
2014-09-28 19:20:55 -0400

    iconSelection

--- Bubble/BBIconSelectionBoard.swift
+++ Bubble/BBIconSelectionBoard.swift
@@ -11,12 +11,40 @@ import UIKit
 
 class BBIconSelectionBoard: UIView{
     
-    var originX:CGFloat = 0
-    var originY:CGFloat = 0
     var originGapx:CGFloat = 20
     var originGapy:CGFloat = 20
     func generateButton(type:BBBoardType){
-        
+        if type == BBBoardType.colorBoard{
+            var originX:CGFloat = 0
+            var originY:CGFloat = 0
+            for var index = 0; index < 8; ++index{
+                var colorBtn: UIButton = UIButton(frame: CGRectMake(originX, originY, 35, 35))
+                let colorPicPath = colorList[index] + ".png"
+                colorBtn.setBackgroundImage(UIImage(named: colorPicPath), forState: .Normal)
+                colorBtn.addTarget(self, action: Selector("colorBtnPressed"), forControlEvents: .TouchUpInside)
+                self.addSubview(colorBtn)
+                originX += 35 + originGapx
+                if index > 2{
+                    originX = 0
+                    originY = 35 + originGapy
+                }
+            }
+        }else{
+            var originX:CGFloat = 0
+            var originY:CGFloat = 200
+            for var index = 0; index < 8; ++index{
+                var iconBtn: UIButton = UIButton(frame: CGRectMake(originX, originY, 35, 35))
+                let iconPicPath = iconList[index] + ".png"
+                iconBtn.setBackgroundImage(UIImage(named: iconPicPath), forState: .Normal)
+                iconBtn.addTarget(self, action: Selector("iconBtnPressed"), forControlEvents: .TouchUpInside)
+                self.addSubview(iconBtn)
+                originX += 35 + originGapx
+                if index > 2{
+                    originX = 0
+                    originY = 35 + originGapy + 200
+                }
+             }
+         }
     }
     
     func getBoardWithType(type:BBBoardType){
